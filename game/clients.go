590ec78f0d214fccffb2874b63535d46c32b5399
Daniel Toye <time4dan@gmail.com>
2015-08-06 23:07:00 -0700

    Added proper level rendering, better ai, networked y axis, etc

--- game/clients.go
+++ game/clients.go
@@ -19,14 +19,67 @@ var nextId = uint32(0)
 var client = map[*Client]*Client{}
 var clientLock sync.RWMutex
 
+var (
+	worldW = 30
+	worldH = 10
+	worldD = 30
+)
+var level [][][]int64
+
+func init() {
+	gen()
+}
+
+func gen() {
+	level = make([][][]int64, worldW)
+	for x := 0; x < worldW; x += 1 {
+		level[x] = make([][]int64, worldH)
+		for y := 0; y < worldH; y += 1 {
+			level[x][y] = make([]int64, worldD)
+		}
+	}
+
+	slope := 10
+	for x := 0; x < worldW; x += 1 {
+		slope += 1 - rand.Intn(3)
+		if slope < 4 {
+			slope = 4
+		}
+		if slope > 8 {
+			slope = 8
+		}
+
+		length := worldD - slope
+		for y := 0; y < worldH; y += 1 {
+			if length < (worldH-y)/2 {
+				length = (worldH - y) / 2
+			}
+			for z := 1; z <= length; z += 1 {
+				level[x][y][z] = 2
+			}
+			length -= 4
+
+		}
+	}
+	for x := 0; x < worldW; x += 1 {
+		for z := 1; z < worldD; z += 1 {
+			level[x][0][z] = 1
+		}
+	}
+
+}
+
 type Entity struct {
-	x      float64
-	y      float64
-	xspeed float64
-	yspeed float64
-	id     uint32
-	dirty  bool
-	owner  *Client
+	x        float64
+	y        float64
+	z        float64
+	xspeed   float64
+	yspeed   float64
+	zspeed   float64
+	id       uint32
+	dirty    bool
+	owner    *Client
+	grounded bool
 }
 
 type Client struct {
@@ -42,6 +95,9 @@ type Client struct {
 }
 
 func (e *Entity) AIChase() {
+	e.z = 20
+	e.y = 10
+	e.x = 20
 	for {
 		time.Sleep(time.Millisecond * 100)
 		nearest := e.nearestEntity()
@@ -50,25 +106,40 @@ func (e *Entity) AIChase() {
 		}
 
 		dx := e.x - nearest.x
-		dy := e.y - nearest.y
-		dist := math.Sqrt(dx*dx + dy*dy)
-		log.Printf("X%f Y%f D%f\n", dx, dy, dist)
+		dz := e.z - nearest.z
+		dist := math.Sqrt(dx*dx + dz*dz)
 
-		if dist > 15 {
-			e.xspeed = (dx / dist) * -3
-			e.yspeed = (dy / dist) * -3
-		} else if dist < 1 {
-			e.xspeed = (rand.Float64() - .5) * 4
-			e.yspeed = (rand.Float64() - .5) * 4
-		} else if dist < 10 {
-			e.xspeed = (dx / dist) * 3
-			e.yspeed = (dy / dist) * 3
+		if dist > 1.5 {
+			e.xspeed = (dx / dist) * -.3
+			e.zspeed = (dz / dist) * -.3
+		} else if dist < .1 {
+			e.xspeed = (rand.Float64() - .5) * .4
+			e.zspeed = (rand.Float64() - .5) * .4
+		} else if dist < 1.0 {
+			e.xspeed = (dx / dist) * .3
+			e.zspeed = (dz / dist) * .3
 		} else {
 			e.xspeed = 0
-			e.yspeed = 0
+			e.zspeed = 0
 		}
 
-		log.Printf("X%f Y%f\n", e.xspeed, e.yspeed)
+		if e.yspeed == 0 {
+			e.y -= 1
+			grounded := e.Colliding()
+			e.y += 1
+			if grounded {
+				e.x += e.xspeed
+				e.z += e.zspeed
+				nearWall := e.Colliding()
+				e.x -= e.xspeed
+				e.z -= e.zspeed
+				if nearWall {
+					e.yspeed = .32
+				}
+			}
+		}
+
+		log.Printf("X%f Z%f\n", e.xspeed, e.zspeed)
 		e.dirty = true
 	}
 }
@@ -80,7 +151,7 @@ func (e *Entity) nearestEntity() (nearest *Entity) {
 	dist := math.Inf(1)
 	for _, ent := range entity {
 		if ent != e && ent != nil {
-			newDist := math.Pow(e.x-ent.x, 2) + math.Pow(e.y-ent.y, 2)
+			newDist := math.Pow(e.x-ent.x, 2) + math.Pow(e.z-ent.z, 2)
 			if newDist < dist {
 				nearest = ent
 				dist = newDist
@@ -94,7 +165,7 @@ func (e *Entity) AIRandom() {
 	for {
 		time.Sleep(time.Second)
 		e.xspeed = (rand.Float64() - .5) * 4
-		e.yspeed = (rand.Float64() - .5) * 4
+		e.zspeed = (rand.Float64() - .5) * 4
 		e.dirty = true
 	}
 }
@@ -104,7 +175,7 @@ var serverTicks = time.NewTicker(tickTime)
 var updates = make(chan func())
 
 func ProcessWorld() {
-	for range [10]struct{}{} {
+	for range [3]struct{}{} {
 		ent := NewEntity()
 		defer ent.Delete()
 		go ent.AIChase()
@@ -171,13 +242,13 @@ func (c *Client) syncWorld() {
 				c.Send("DEL", id)
 			} else {
 				if ent.dirty {
-					c.Send("MOVE", id, ent.x, ent.y, ent.xspeed, ent.yspeed)
+					c.Send("MOVE", id, ent.x, ent.y, ent.z, ent.xspeed, ent.yspeed, ent.zspeed)
 				}
 			}
 		} else {
 			if ent != nil {
 				c.Send("NEW", id, "PLAYER")
-				c.Send("MOVE", id, ent.x, ent.y, ent.xspeed, ent.yspeed)
+				c.Send("MOVE", id, ent.x, ent.y, ent.z, ent.xspeed, ent.yspeed, ent.zspeed)
 				c.knownEnts[id] = struct{}{}
 				if ent.owner == c {
 					c.Send("CONTROL", id)
@@ -200,6 +271,9 @@ func (c *Client) Connect(s *websocket.Conn) {
 	log.Println("+", c.origin)
 
 	ent := NewEntity()
+	ent.x = 20
+	ent.y = 20
+	ent.z = 20
 	ent.owner = c
 	c.owns = ent
 
@@ -207,6 +281,7 @@ func (c *Client) Connect(s *websocket.Conn) {
 	client[c] = c
 	clientLock.Unlock()
 
+	c.Send("WORLD", level)
 	go c.Pings()
 	c.ReadForever()
 }
@@ -265,8 +340,10 @@ func (c *Client) ReadForever() {
 					ent.dirty = true
 					ent.x = msg[2].(float64)
 					ent.y = msg[3].(float64)
-					ent.xspeed = msg[4].(float64)
-					ent.yspeed = msg[5].(float64)
+					ent.z = msg[4].(float64)
+					ent.xspeed = msg[5].(float64)
+					ent.yspeed = msg[6].(float64)
+					ent.zspeed = msg[7].(float64)
 				} else {
 					log.Println("HACK ATTEMPT", c.origin)
 				}
@@ -326,20 +403,79 @@ func (e *Entity) Delete() {
 }
 
 func (e *Entity) Simulate() {
+	playerSize := .5
+	gravity := -.05
 	e.x += e.xspeed
+	if e.Colliding() {
+		e.y += .3
+		if e.Colliding() {
+			e.y -= .3
+			e.x -= e.xspeed
+		}
+	}
+	e.z += e.zspeed
+	if e.Colliding() {
+		e.y += .3
+		if e.Colliding() {
+			e.y -= .3
+			e.z -= e.zspeed
+		}
+	}
+	e.yspeed += gravity
 	e.y += e.yspeed
-	if e.x > 297 {
-		e.x = 297
+	if e.Colliding() {
+		e.y -= e.yspeed
+		if e.yspeed < 0 {
+			e.grounded = true
+		}
+		e.yspeed = 0
+	} else {
+		e.grounded = false
 	}
-	if e.y > 197 {
-		e.y = 197
+
+	if e.x < playerSize/2 {
+		e.x = playerSize / 2
+		e.xspeed = 0
 	}
-	if e.x < 3 {
-		e.x = 3
+	if e.x > float64(worldW)+1-playerSize/2 {
+		e.x = float64(worldW) + 1 - playerSize/2
+		e.xspeed = 0
 	}
-	if e.y < 3 {
-		e.y = 3
+	if e.z < playerSize/2 {
+		e.z = playerSize / 2
+		e.zspeed = 0
 	}
+	if e.z > float64(worldD)+1-playerSize/2 {
+		e.z = float64(worldD) + 1 - playerSize/2
+
+	}
+}
+
+func (e *Entity) Colliding() bool {
+	playerSize := .5
+	sx := int(math.Floor(e.x - playerSize/2))
+	sy := int(math.Floor(e.y - playerSize/2))
+	sz := int(math.Floor(e.z - playerSize/2))
+	lx := int(math.Ceil(e.x + playerSize/2))
+	ly := int(math.Ceil(e.y + playerSize/2))
+	lz := int(math.Ceil(e.z + playerSize/2))
+	if sx < 0 || lx >= worldW || sz < 0 || lz >= worldD {
+		return true
+	}
+	if sy < 0 || ly >= worldH {
+		return false
+	}
+	for ix := sx; ix < lx; ix += 1 {
+		for iy := sy; iy < ly; iy += 1 {
+			for iz := sz; iz < lz; iz += 1 {
+				blockType := level[ix][iy][iz]
+				if blockType > 0 {
+					return true
+				}
+			}
+		}
+	}
+	return false
 }
 
 func deleteOldEnts() {
